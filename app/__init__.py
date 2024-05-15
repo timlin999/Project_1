e048ba4ef5def1c7057a69ab0f589e3e3352ee5a
timlin999 <ustinovmail@mail.ru>
2024-05-15 11:01:24 +0400

    1

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+from . import models, views
+
 app = Flask(__name__)
 app.config.from_object('config.Config')
 
@@ -10,7 +12,5 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message = 'Неоходимо авторизоваться'
 login_manager.login_message_category = 'alert-warning'
-
-from . import models, views
 
 db.create_all()
